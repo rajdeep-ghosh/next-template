cac07b52f7fe25614cde71f0aa53b5ac1d69d1bf
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2023-03-24 02:56:48 +0530

    setup eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <>
       <Head>
