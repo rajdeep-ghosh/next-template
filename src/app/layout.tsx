ff7acec509ccb3faca1d1038f700b3dea0f61b3e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2024-03-05 07:39:25 +0530

    update template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import '@/styles/global.css';
+import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Next Js Template',
@@ -8,12 +11,10 @@ export const metadata: Metadata = {
     'Created using the Next template by Rajdeep Ghosh (https://github.com/rajdeep-ghosh/next-template)'
 };
 
-const RootLayout = ({ children }: PropsWithChildren) => {
+export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
-};
-
-export default RootLayout;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
