e7192d69f275ad8c4f66004bdcf5c3bf79ae08b7
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-08-31 19:31:43 +0530

    switch to using prettier flat file config

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  semi: true,
+  plugins: ['prettier-plugin-tailwindcss']
+};
+
+export default config;
