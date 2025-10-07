a6d63c85f5d0c6d7082b371fc928771d3537100b
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2025-10-07 17:11:01 +0530

    update prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -4,7 +4,37 @@ const config = {
   jsxSingleQuote: true,
   trailingComma: 'none',
   semi: true,
-  plugins: ['prettier-plugin-tailwindcss']
+  importOrder: [
+    '^(react/(.*)$)|^(react$)|^(react-dom$)',
+    '^(next/(.*)$)|^(next$)|^(@next/(.*)$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/app/(.*)$',
+    '^@/pages/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '',
+    '^[./]',
+    '',
+    '<TYPES>^(node:)',
+    '<TYPES>',
+    '<TYPES>^@/types(?:/(.*))?$',
+    '<TYPES>^[.]'
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: false,
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss'
+  ]
 };
 
 export default config;
