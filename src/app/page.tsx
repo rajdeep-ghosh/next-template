0c5293ce354e3170d1d722c884a54aa40ec63a09
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2026-06-23 13:36:19 +0530

    use proper style classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function HomePage() {
   return (
     <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20'>
-      <main className='row-start-2 flex flex-col items-center gap-[32px] sm:items-start'>
+      <main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
         <Image
           className='dark:invert'
           src='/next.svg'
@@ -15,7 +15,7 @@ export default function HomePage() {
         <ol className='list-inside list-decimal text-center font-mono text-sm/6 sm:text-left'>
           <li className='mb-2 tracking-[-.01em]'>
             Get started by editing{' '}
-            <code className='rounded bg-black/[.05] px-1 py-0.5 font-mono font-semibold dark:bg-white/[.06]'>
+            <code className='rounded bg-black/5 px-1 py-0.5 font-mono font-semibold dark:bg-white/6'>
               src/app/page.tsx
             </code>
             .
